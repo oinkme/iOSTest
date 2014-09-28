c9cff4ba8c70d6ece25978c008c572ca917e7055
oinkme <munoz.donoso@gmail.com>
2014-09-27 22:57:09 -0300

    animations

--- Prototype/ViewController.m
+++ Prototype/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *dialogView;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+- (IBAction)loginButtonDidPress:(id)sender;
 
 @end
 
@@ -22,14 +25,30 @@
     [self setNeedsStatusBarAppearanceUpdate];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 - (UIStatusBarStyle)preferredStatusBarStyle{
     return UIStatusBarStyleLightContent;
 }
 
+- (IBAction)loginButtonDidPress:(id)sender {
+    // Animate login button
+    [UIView animateWithDuration:0.1 animations:^{
+        self.loginButton.transform = CGAffineTransformMakeTranslation(10, 0);
+    } completion:^(BOOL finished) {
+        // Step 2
+        [UIView animateWithDuration:0.1 animations:^{
+            self.loginButton.transform = CGAffineTransformMakeTranslation(-10, 0);
+        } completion:^(BOOL finished) {
+            //Step 3
+            [UIView animateWithDuration:0.1 animations:^{
+                self.loginButton.transform = CGAffineTransformMakeTranslation(0, 0);
+            }];
+        }];
+    }];
+    
+    // animateWithDuration with Damping
+    [UIView animateWithDuration:0.7 delay:0 usingSpringWithDamping:0.5 initialSpringVelocity:0 options:0 animations:^{
+        // Change the height of the dialogView
+        [self.dialogView setFrame:CGRectMake(self.dialogView.frame.origin.x, self.dialogView.frame.origin.y, self.dialogView.frame.size.width, 320)];
+    } completion:^(BOOL finished) { }];
+}
 @end
